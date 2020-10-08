c433908b64818a01f9e58805f319e0c7a38b66d3
Tejumola Mann <tejumola.mann@misterichconsult.com>
2020-10-08 22:06:05 +0100

    Created method createMapIntent()
    Made textView address clickable and added createMapIntent() for onclick

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -26,6 +28,17 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent intentMap = new Intent(Intent.ACTION_VIEW, uriAddress);
+
+        intentMap.setPackage("com.google.android.apps.maps");
+
+        if (intentMap.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentMap);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
